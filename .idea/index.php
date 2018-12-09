ed24f42dee60d11ea890e2af0cb91f27baeb9da2
Akhara <43671530+Student2003@users.noreply.github.com>
2018-12-09 13:52:12 +0100

    essai

---
+++ .idea/index.php
@@ -0,0 +1,40 @@
+<?php
+define("ROOT", __DIR__);
+
+require ROOT . "/controllers/controller.php";
+if (isset($_GET["action"])) {
+    $action = htmlspecialchars($_GET["action"]);
+
+    switch ($action){
+        case "prise_de_rdv":
+            prise_de_rdv();
+            break;
+
+        case "signaler_une_panne":
+            formulaire_panne();
+            break;
+
+        case "faq":
+            questions_frequentes();
+            break;
+
+        case "contact":
+            contact();
+            break;
+
+        default :
+            echo"error 404";
+            break;
+    }
+}
+else {
+    seeHome();
+}
+?>
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Akhara
+ * Date: 09/12/2018
+ * Time: 13:51
+ */
